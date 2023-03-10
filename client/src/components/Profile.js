528ad299dcaf848fc8f83d03fa7a3fc12534a29c
SamuelFang <samuelfang02@gmail.com>
2023-03-10 01:01:54 -0500

    edit now works

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { editProfile } from "../api/profile";
 import Layout from "./Layout";
 import { useParams } from "react-router-dom";
 import { getProfile } from "../api/profile";
-import EditProfile from "./EditProfile";
 
 import '../styles/Profile.css';
 
@@ -16,13 +16,38 @@ const Profile = () => {
     city: '',
     bio: '',
     education: '',
-    hobbies: '',
+    hobbies: ''
   })
 
   const [editMode, setEditMode] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const onChange = (e) => {
+    setUserInfo({...userInfo, [e.target.name]: e.target.value})
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await editProfile(id, userInfo)
+      setError('')
+      setSuccess(res.data.message)
+      setEditMode(false);
+
+    } catch (err) {
+      console.error(err.response.data.errors[0].msg)
+      setError(err.response.data.errors[0].msg)
+      setSuccess('')
+    }
+
+    
+  }
 
   const cancelEdit = () => {
     setEditMode(false);
+    setSuccess()
   }
 
   const retrieveInfo = async () => {
@@ -40,13 +65,36 @@ const Profile = () => {
     <Layout>
       {editMode ? (
         <div>
-          <EditProfile cancelEdit={cancelEdit}/>
+        <div class ="name"> Edit Profile </div>
+        <div class ="description">
+          Username: <input onChange={(e) => onChange(e)} id="profile_name" name="profile_name" type='text' placeholder="Username" required />
+        </div>
+        <div class ="description">
+          Bio: <input onChange={(e) => onChange(e)} id="bio" name="bio" type='text' placeholder="Bio" required />
+        </div>
+        <div class ="description">
+          City: <input onChange={(e) => onChange(e)} id="city" name="city" type='text' placeholder="City" required />
+        </div>
+        <div class ="description">
+          Education: <input onChange={(e) => onChange(e)} id="education" name="education" type='text' placeholder="Education" required />
+        </div>
+        <div class ="description">
+          Hobbies: <input onChange={(e) => onChange(e)} id="hobbies" name="hobbies" type='text' placeholder="Hobbies" required />
+        </div>
+        <br></br>
+        <div class ="email">
+            <button type="submit" className="form-btn btn btn-primary" onClick={() => cancelEdit()}> Cancel</button>
+            <br></br>
+            <button type="submit" className="form-btn btn btn-primary" onClick={(e) => onSubmit(e)}> Submit</button>
+        </div>
+        <div class = "description" style={{color:'red', margin: '10px 0' }}>{error}</div>
         </div>
       ) : (
         <div>
           <div class ="name"> {userInfo.profile_name}
-          {userInfo.profile_id == 1 ? (<button onClick={() => setEditMode(true)}> <i class="fas fa-pen"> </i> </button>) : (null)}
+          {userInfo.profile_id === 1 ? (<button onClick={() => setEditMode(true)}> <i class="fas fa-pen"> </i> </button>) : (null)}
           </div>
+          <div class = "description" style={{color:'green', margin: '10px 0' }}>{success}</div>
           <div class ="email"><i class="fas fa-envelope"></i> {userInfo.profile_email} </div>
           <br></br>
           {userInfo.bio != null ? (<div class ="description"> {userInfo.bio} </div>) : (<div class ="description">No Description Listed</div>)}
