2ea785b3916015320f843015ac115f91a33e9559
bh9232 <bhong9232@gmail.com>
2023-03-16 15:30:39 -0400

    removed unused code

--- client/src/components/Posts/Post.js
+++ client/src/components/Posts/Post.js
@@ -7,22 +7,6 @@ import Modal from "../Modal";
 
 import '../../styles/Post.css';
 
-// document.addEventListener("click", e => {
-//   const isDropDownButton = e.target.matches("[data-dropdown-button]")
-//   if(!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
-
-//   let currentDropDown
-//   if(isDropDownButton) {
-//     currentDropDown = e.target.closest('[data-dropdown]')
-//     currentDropDown.classList.toggle('active')
-//   }
-
-//   document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-//     if(dropdown === currentDropDown) return
-//     dropdown.classList.remove('active')
-//   })
-// })
-
 const Post = (props) => {
   const { post_id, post_name, description_text, likes, comments } = props.postData;
   const reloadPosts = props.reloadPosts;
