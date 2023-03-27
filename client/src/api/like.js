1727bcfce12f77a4a7758b122caae1665bcc50d2
Jeffrey Li <lijeffrey02@gmail.com>
2023-03-27 18:35:05 -0400

    change for hosting

--- client/src/api/like.js
+++ client/src/api/like.js
@@ -4,30 +4,30 @@ axios.defaults.withCredentials = true
 
 // Get Posts Liked By User
 export async function getPostsLikedByUser(id) {
-  return await axios.get(`http://localhost:8000/api/likeposts/${id}`)
+  return await axios.get(`http://mendinghealth.space/api/likeposts/${id}`)
 }
 
 // Like Post From User
 export async function likePostFromUser(likeData) {
-  return await axios.post('http://localhost:8000/api/likepost', likeData)
+  return await axios.post('http://mendinghealth.space/api/likepost', likeData)
 }
 
 // Unlike Post From User
 export async function unlikePostFromUser(id) {
-  return await axios.delete(`http://localhost:8000/api/unlikepost/${id}`)
+  return await axios.delete(`http://mendinghealth.space/api/unlikepost/${id}`)
 }
 
 // Get Comments Liked By User
 export async function getCommentsLikedByUser(id) {
-  return await axios.get(`http://localhost:8000/api/likecomments/${id}`)
+  return await axios.get(`http://mendinghealth.space/api/likecomments/${id}`)
 }
 
 // Like Comment From User
 export async function likeCommentFromUser(likeData) {
-  return await axios.post('http://localhost:8000/api/likecomment', likeData)
+  return await axios.post('http://mendinghealth.space/api/likecomment', likeData)
 }
 
 // Unlike Comment From User
 export async function unlikeCommentFromUser(id) {
-  return await axios.delete(`'http://localhost:8000/api/unlikecomment/${id}`)
+  return await axios.delete(`'http://mendinghealth.space/api/unlikecomment/${id}`)
 }
--- client/src/api/post.js
+++ client/src/api/post.js
@@ -4,35 +4,35 @@ axios.defaults.withCredentials = true
 
 // Get Posts
 export async function getPosts() {
-  return await axios.get('http://localhost:8000/api/posts')
+  return await axios.get('http://mendinghealth.space/api/posts')
 }
 
 // Get Post by Id
 export async function getPostById(id) {
-  return await axios.get(`http://localhost:8000/api/posts/${id}`)
+  return await axios.get(`http://mendinghealth.space/api/posts/${id}`)
 }
 
 // Create Post
 export async function createPost(postData) {
-  return await axios.post('http://localhost:8000/api/newpost', postData)
+  return await axios.post('http://mendinghealth.space/api/newpost', postData)
 }
 
 // Edit Post
 export async function editPost(id, postData) {
-  return await axios.put(`http://localhost:8000/api/editpost/${id}`, postData)
+  return await axios.put(`http://mendinghealth.space/api/editpost/${id}`, postData)
 }
 
 // Add Like to Post
 export async function addLikePost(id) {
-  return await axios.put(`http://localhost:8000/api/addlikepost/${id}`)
+  return await axios.put(`http://mendinghealth.space/api/addlikepost/${id}`)
 }
 
 // Remove Like from Post
 export async function removeLikePost(id) {
-  return await axios.put(`http://localhost:8000/api/removelikepost/${id}`)
+  return await axios.put(`http://mendinghealth.space/api/removelikepost/${id}`)
 }
 
 // Delete Post
 export async function deletePost(id) {
-  return await axios.delete(`http://localhost:8000/api/deletepost/${id}`)
+  return await axios.delete(`http://mendinghealth.space/api/deletepost/${id}`)
 } 
