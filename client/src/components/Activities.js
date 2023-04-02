dcded6d69934bf0316b7093b137e1751789e65db
TommyLiang1 <tommyliang131@gmail.com>
2023-04-02 15:56:56 -0400

    Activity card flip individually not all together

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
+import Layout from "./Layout";
 import List from "../ActivityList.json"
 import "../styles/Activities.css";
 
-
 const Activities = () => {
-  const [flip, setFlip] = useState(false)
-  
   return(
-    <div>
-      <h1>Hello World</h1>
+    <Layout>   
+      <h1 className="activity-header">Mental Health Activities</h1>
       <br></br>
-      {
-        List.map(activity => {
-          return(
-            <div className={`card ${flip ? "flip" : ""}`} key={activity.id} onClick={() => setFlip(!flip)}>
-              <div className="front">
-                {activity.name}
-              </div>
-              <div className="back">
-                {activity.detail}
-              </div>
-            </div>
-          )
-        })
-      }
+      <div className="card-grid">
+        {
+          List.map(activity => {
+            return(
+              <Activity key={activity.id} name={activity.name} detail={activity.detail}/>
+            )
+          })
+        }
+      </div>
+    </Layout>
+  )
+}
+
+const Activity = (props) => {
+  const [flip, setFlip] = useState(false)
+  return(
+    <div className={`card ${flip ? "flip" : ""}`} onClick={() => setFlip(!flip)}>
+      <div className="front">
+        {props.name}
+      </div>
+      <div className="back">
+        {props.detail}
+      </div>
     </div>
   )
 }
